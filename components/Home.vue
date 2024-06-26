122e256085198321a79ec23a9c1528ed75c90560
Maicon Alves <AD648ZZ@mmm.com>
2024-06-26 14:54:44 -0300

    Style: Update grid system

--- components/Home.vue
+++ components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid">
-        <div class="flex flex-column col-6">
+        <div class="flex flex-column col-12 md:col-6">
             <span class="text-4xl font-bold">Olá, eu sou o <strong class="text-green-300">Maicon Alves</strong></span>
             <span class="text-4xl font-bold">Sou um Desenvolvedor Fullstack Pleno.</span>
             <p class="text-md">
@@ -15,16 +15,16 @@
                 <Button class="ml-2 p-button-lg" severity="secondary" label="Projetos" @click="redirectToGithub"/>
             </div>
         </div>
-        <div class="col-6 border-round-sm flex justify-content-center">
+        <div class="col-12 md:col-6 border-round-sm flex justify-content-center">
             <img src="../assets/images/Developer.png" class="w-6">
         </div>
     </div>
     <Divider />
     <div class="grid">
-        <div class="col-6 border-round-sm flex justify-content-center">
+        <div class="col-12 md:col-6 border-round-sm flex justify-content-center">
             <img src="../assets/images/Developer.png" class="w-6">
         </div>
-        <div class="flex flex-column col-6">
+        <div class="flex flex-column col-12 md:col-6">
             <span class="text-4xl font-bold"><strong class="text-green-300">Sobre mim</strong></span>
             <p class="text-md">
                 Sou um estudante de Engenharia de Computação com experiência em desenvolvimento FullStack desde 2022.
@@ -40,22 +40,22 @@
     </div>
     <Divider />
     <div class="grid">
-        <div class="flex flex-column col-6">
+        <div class="flex flex-column col-12 md:col-6">
             <span class="text-4xl font-bold"><strong class="text-green-300">Experiências profissionais</strong></span>
             <p class="text-md">
                 Na experiência de Desenvolvedor Fullstack na 3M, desempenhei um papel fundamental no processo de concepção e arquitetura de sistemas robustos, proporcionando avanços significativos nas operações logísticas da empresa. Esses sistemas, desenhados com a precisão que a área demanda, beneficiaram milhares de usuários e resultaram em economias substanciais para a empresa. Além das economias financeiras diretas, os sistemas que desenvolvi também trouxeram uma melhoria na eficiência operacional. Ao otimizar os processos, consegui reduzir significativamente o tempo necessário para as tarefas dos usuários. Esta otimização não apenas economizou recursos financeiros, mas também liberou o valioso tempo dos nossos usuários, permitindo-lhes focar em outras atividades estratégicas. 
             </p>
         </div>
-        <div class="col-6 border-round-sm flex justify-content-center">
+        <div class="col-12 md:col-6 border-round-sm flex justify-content-center">
             <img src="../assets/images/3m.jpg" class="w-6">
         </div>
     </div>
     <Divider />
     <div class="grid">
-        <div class="col-6 border-round-sm flex justify-content-center">
+        <div class="col-12 md:col-6 border-round-sm flex justify-content-center">
             <img src="../assets/images/respirar.png" class="w-6">
         </div>
-        <div class="flex flex-column col-6">
+        <div class="flex flex-column col-12 md:col-6">
             <span class="text-4xl font-bold"><strong class="text-green-300">Projetos desenvolvidos - RespirAr</strong></span>
             <p class="text-md">
                 O projeto "RespirAr" surge como uma iniciativa inovadora no campo da saúde pública e tecnologia ambiental, mirando o desenvolvimento e a implementação de um sistema avançado de monitoramento e alerta da qualidade do ar, especialmente para ambientes de ensino público. Este sistema promete transformar o ambiente educacional, tornando-o mais saudável e seguro para estudantes e professores. Com a utilização de tecnologias de ponta, o projeto visa detectar uma ampla gama de poluentes - como dióxido de carbono, amônia e partículas alergênicas - fornecendo dados em tempo real essenciais para a tomada de decisões e a manutenção de um ar puro nas salas de aula.            
