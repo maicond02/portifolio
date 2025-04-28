6aeb6f5f9a772e1c876b4d0f7acc3b5c158f6a81
Maicon Alves <eumaicondouglas.md45@gmail.com>
2025-04-28 14:42:01 -0300

    feat: Create navigation on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,52 @@
-
+'use client';
 import React from 'react'; 
 import { Menubar } from 'primereact/menubar';
 import { MenuItem } from 'primereact/menuitem';
-import styles from '../styles/navbar.module.css'
+import { Button } from 'primereact/button';
+import { useRouter } from 'next/navigation'
 export default function BasicDemo() {
+    const router = useRouter();
     const items: MenuItem[] = [
         {
             label: 'Home',
-            icon: 'pi pi-home'
+            icon: 'pi pi-home',
+            url:'/page'
         },
         {
             label: 'About',
-            icon: 'pi pi-star'
+            icon: 'pi pi-info-circle',
+            url:'/about'
         },
         {
             label: 'Projects',
-            icon: 'pi pi-envelope'
+            icon: 'pi pi-star',
+            url:'/projects'
         },
         {
             label: 'Skills',
-            icon: 'pi pi-envelope'
+            icon: 'pi pi-book',
+            url:'/skills'
         },
         {
             label: 'Contact',
-            icon: 'pi pi-envelope'
+            icon: 'pi pi-linkedin',
+            url:'https://www.linkedin.com/in/maicon-alves-03700419b/'
         },
     ];
+
+    const end = (
+        <div className="flex align-items-center gap-2">
+            <Button label="Download CV" icon="pi pi-download" onClick={donwloadCV} />
+        </div>
+    );
+
+    function donwloadCV(){
+        window.location.href = 'https://github.com'
+    }
     
     return (
         <div>
-            <Menubar model={items} />
+            <Menubar model={items} end={end}/>
         </div>
     )
 }
