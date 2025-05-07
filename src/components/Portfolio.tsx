acaf25ea230518d9e2df50e05cfd54f70e2ce17b
Maicon Alves <eumaicondouglas.md45@gmail.com>
2025-05-07 14:44:17 -0300

    Feat: Portfolio header

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,17 +1,42 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from 'primereact/button';
-import { Card } from 'primereact/card';
-import Style from  "../styles/about.module.css";
+import Styles from '../styles/portfolio.module.css'
 
 export default function Portfolio() {
     const router = useRouter();
-    
+    const [active, setActive] = useState('Projects');
+    const selectButton = (label:string) => {
+        setActive(label);
+    }
+    const buttons = [
+        { icon: 'pi pi-star', label: 'Projects' },
+        { icon: 'pi pi-book', label: 'Certificates' },
+        { icon: 'pi pi-globe', label: 'Career' },
+      ];
     return (
         <div>
             <div className='flex justify-center'>
                 <h2 className='text-6xl font-bold text-cyan-300 leading-tight mb-6'>Portfolio</h2>
+            </div>
+            <div className='flex gap-4 justify-center'>
+            <span className={Styles.card}>
+                {buttons.map(({ icon, label }) => {
+                    const isSelected = active === label;
+                    return (
+                    <Button
+                        key={label}
+                        icon={icon}
+                        label={label}
+                        className="w-xl h-24"
+                        outlined={isSelected}
+                        text={!isSelected}
+                        onClick={() => selectButton(label)}
+                    />
+                    );
+                })}
+            </span>
             </div>
         </div>
     );
